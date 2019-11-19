6fb48cc8da2c39b0fd9508dd2da31bb4cc56e7d1
Adam Chipperfield <adam.chipperfield@wemakewebsites.com>
2019-11-19 13:33:42 +0000

    Added the product gallery component

---
+++ components/ProductGallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="product-gallery">
+    <img
+      v-for="(image, index) in images"
+      :key="index"
+      :alt="image.altText"
+      :src="image.transformedSrc"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    images: Object,
+  },
+}
+</script>
--- pages/products/_handle.vue
+++ pages/products/_handle.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="template-product">
-    <product-form :product="product" />
+    <div class="row">
+      <div class="col xs12 m6">
+        <product-gallery :images="images" />
+      </div>
 
-    <div v-html="product.descriptionHtml"></div>
+      <div class="col xs12 m6">
+        <product-form :product="product" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import productByHandle from '../../graphql/queries/productByHandle';
 import ProductForm from '../../components/ProductForm';
+import ProductGallery from '../../components/ProductGallery';
 
 export default {
   components: {
     ProductForm,
+    ProductGallery,
   },
   async asyncData({ app, params, store }) {
     const client = app.apolloProvider.defaultClient;
@@ -24,6 +32,7 @@ export default {
 
     return {
       product: product.data.productByHandle,
+      images: product.data.productByHandle.images.edges.map((item) => item.node),
     }
   },
 }
