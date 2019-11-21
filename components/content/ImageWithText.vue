ee2edabb948f4347c855a355876d00e76841761a
Adam Chipperfield <adamchipperfield01@gmail.com>
2019-11-21 13:24:14 +0000

    Built the image with text section

---
+++ components/content/ImageWithText.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="image-with-text">
+    <div class="container">
+      <div class="row flex">
+        <div class="col xs12 m6">
+          <img
+            v-if="entry.fields.image"
+            :alt="entry.fields.image.fields.title"
+            :src="getImageUrl(entry.fields.image)"
+            class="image-with-text__image"
+          >
+        </div>
+
+        <div class="col xs12 m6">
+          <div class="image-with-text__body">
+            <h3
+              v-if="entry.fields.title"
+              class="image-with-text__title"
+            >
+              {{ entry.fields.title }}
+            </h3>
+
+            <p
+              v-if="entry.fields.content"
+              class="image-with-text__content"
+            >
+              {{ entry.fields.content }}
+            </p>
+
+            <a
+              v-if="entry.fields.button.fields.label"
+              :href="entry.fields.button.fields.url"
+              class="button button--outline"
+            >
+              {{ entry.fields.button.fields.label }}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '~/assets/styles/components/content/image-with-text';
+</style>
+
+<script>
+export default {
+  props: {
+    entry: Object,
+  },
+  methods: {
+    getImageUrl(image) {
+      return image.fields.file.url;
+    },
+  },
+}
+</script>
