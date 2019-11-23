b1f914d992d1a63c78d0090b441de5701b5812e2
Adam Chipperfield <adam.chipperfield@wemakewebsites.com>
2019-11-23 16:32:06 +0000

    Added the access template stylesheet and basic layout

--- pages/account/access.vue
+++ pages/account/access.vue
@@ -1,46 +1,57 @@
 <template>
   <div class="template-access">
-    <form @submit="handleLoginSubmit">
-      <label for="Email">{{ $t('account.login.email') }}</label>
-      <input
-        ref="emailInput"
-        id="Email"
-        type="email"
-        placeholder="Email Address"
-      >
+    <div class="container">
+      <div class="row">
+        <div class="col xs12 m6">
+          <form @submit="handleLoginSubmit">
+            <label for="Email">{{ $t('account.login.email') }}</label>
+            <input
+              ref="emailInput"
+              id="Email"
+              type="email"
+              placeholder="Email Address"
+            >
 
-      <label for="Password">{{ $t('account.login.password') }}</label>
-      <input
-        ref="passwordInput"
-        id="Password"
-        type="password"
-        placeholder="Password"
-      >
+            <label for="Password">{{ $t('account.login.password') }}</label>
+            <input
+              ref="passwordInput"
+              id="Password"
+              type="password"
+              placeholder="Password"
+            >
 
-      <button type="submit">{{ $t('account.login.login') }}</button>
-    </form>
+            <button type="submit">{{ $t('account.login.login') }}</button>
+          </form>
+        </div>
+        <div class="col xs12 m6">
+          <form @submit="handleRegisterSubmit">
+            <label for="Email">{{ $t('account.register.email') }}</label>
+            <input
+              ref="emailInput"
+              id="Email"
+              type="email"
+              placeholder="Email Address"
+            >
 
-    <form @submit="handleRegisterSubmit">
-      <label for="Email">{{ $t('account.register.email') }}</label>
-      <input
-        ref="emailInput"
-        id="Email"
-        type="email"
-        placeholder="Email Address"
-      >
+            <label for="Password">{{ $t('account.register.password') }}</label>
+            <input
+              ref="passwordInput"
+              id="Password"
+              type="password"
+              placeholder="Password"
+            >
 
-      <label for="Password">{{ $t('account.register.password') }}</label>
-      <input
-        ref="passwordInput"
-        id="Password"
-        type="password"
-        placeholder="Password"
-      >
-
-      <button type="submit">{{ $t('account.register.createAccount') }}</button>
-    </form>
+            <button type="submit">{{ $t('account.register.createAccount') }}</button>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+@import '~/assets/styles/pages/account/access';
+</style>
 
 <script>
 export default {
