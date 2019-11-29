b4ae1a04533e1c346b02d499ac1a85822a87049c
Adam Chipperfield <adam.chipperfield@wemakewebsites.com>
2019-11-29 15:19:20 +0000

    Added French translations

--- locales/fr-FR.js
+++ locales/fr-FR.js
@@ -1,15 +1,87 @@
 export default {
-  account: {
-    order: {
-      orderNumber: 'Numéro de commande',
-      date: 'Date',
-      products: 'Des produits',
-      total: 'Montant total',
+  product: {
+    form: {
+      variant: 'Une variante',
+      quantity: 'Qté',
+      addToCart: 'Ajouter au panier',
+      addingToCart: 'Ajout au panier...',
+      outOfStock: 'En rupture de stock',
+      selectOption: 'Sélectionnez la #option#',
     },
   },
   collection: {
     filter: {
       clear: 'Effacer les filtres',
     },
+  },
+  checkout: {
+    title: 'Caisse',
+  },
+  cartDrawer: {
+    title: 'Tiroir de chariot',
+    checkout: 'Commander en toute sécurité',
+    lineItems: {
+      remove: 'Retirer',
+    },
+  },
+  currencySelector: {
+    label: 'Sélecteur de devise',
+  },
+  languageSelector: {
+    label: 'Sélecteur de langue',
+  },
+  account: {
+    title: 'Mon compte',
+    logout: 'Déconnexion',
+    welcome: 'Accueillir, #name#.',
+    order: {
+      orderNumber: 'Numéro de commande',
+      date: 'Date',
+      products: 'Des produits',
+      total: 'Montant total',
+      no_orders: 'Vous n\'avez pas encore passé de commande.',
+    },
+    login: {
+      title: 'Connexion',
+      email: 'Adresse e-mail',
+      password: 'Mot de passe',
+      login: 'Connexion',
+      reset_password: 'Mot de passe oublié',
+      resetting_password: 'Travaillant...',
+      sent_reset_password: 'Email de récupération envoyé',
+      submit_reset_password: 'Mot de passe oublié',
+      close_reset_password: 'Connexion',
+    },
+    register: {
+      title: 'Créer un compte',
+      line_1: 'Nous ne sauvegardons jamais les informations de carte de crédit.',
+      line_2: 'L\'enregistrement rend la commande rapide et facile et enregistre les informations de votre commande dans votre compte.',
+      email: 'Adresse e-mail',
+      password: 'Mot de passe',
+      createAccount: 'Créer un compte',
+    },
+    addresses: {
+      add: 'Ajouter une adresse',
+      edit: 'Modifier',
+      delete: 'Supprimer',
+      save: 'Enregistrer',
+      saving: 'Économie...',
+      deleting: 'Suppression...',
+      firstName: 'Prénom',
+      lastName: 'Nom de famille',
+      company: 'Entreprise',
+      address1: 'Adresse',
+      address2: 'Appartement, suite, etc.',
+      city: 'Ville',
+      country: 'Pays',
+      zip: 'Code postal',
+      phone: 'Téléphone',
+      no_addresses: 'Vous n\'avez pas encore ajouté d\'adresse.',
+    },
+  },
+  error: {
+    title: 'Uh-oh, rien à voir ici!',
+    subtitle: 'La page que vous avez demandée n\'existe pas.',
+    link_label: 'Rentrer chez soi',
   },
 }
